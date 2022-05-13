ef09f53289b2785f485669f9786ddc7837e4294c
NicolasVaillant <see_f20nvail@etu.emse.fr>
2022-05-13 18:21:07 +0200

    Design Updated

--- js/script.js
+++ js/script.js
@@ -27,6 +27,7 @@ async function addData(label, data, weather) {
     myChart.data.labels.push(label);
     myChart.data.datasets[0].data.push(Number(data))
     myChart.data.datasets[1].data.push(Number(weather))
+    myChart.data.datasets[2].data.push(Number(19))
     myChart.update();
 }
 
@@ -38,19 +39,37 @@ const myChart = new Chart(ctx, {
             label: 'Chambre',
             data: [],
             borderColor: ['rgba(54, 162, 235, 1)'],
-            borderWidth: 2
+            borderWidth: 2,
+            fill: true,
+            backgroundColor : ['rgba(121,182,224,0.37)'],
+            pointBackgroundColor : ['rgba(54, 162, 235, 1)'],
+            order:3
         }, {
             label: 'Ploemel (ext)',
             data: [],
             borderColor: ['rgb(211, 54, 235, 1)'],
-            borderWidth: 2
+            borderWidth: 2,
+            fill: true,
+            backgroundColor : ['rgba(217,114,232,0.6)'],
+            pointBackgroundColor : ['rgb(211, 54, 235, 1)'],
+            order:2
+        }, {
+            label: 'Idéal chambre',
+            data: [],
+            borderColor: ['rgb(41,224,83)'],
+            fill: false,
+            backgroundColor : ['rgb(255,255,255,1)'],
+            pointBackgroundColor : ['rgb(41,224,83)'],
+            borderWidth: 2,
+            borderDash: [10,5],
+            order:1
         }]
     },
     options: {
         scales: {
             y: {
                 suggestedMin: 10,
-                suggestedMax: 30
+                suggestedMax: 30,
             }
         },
         responsive:true
